25181274b9358788d694bfe4aa1c9491554f2c05
dzexpressonlineshop-sudo <dzexpressonlineshop@gmail.com>
2026-06-20 21:47:56 +0200

    config.js

--- js/config.js
+++ js/config.js
@@ -5,6 +5,6 @@ const CONFIG = {
   storeName: "DZ Express Online Shop",
   // رابط Google Apps Script Web App (شرح الحصول عليه في README.md)
   // مثال: "https://script.google.com/macros/s/AKfycb.../exec"
-  APPS_SCRIPT_URL: "",
+  APPS_SCRIPT_URL: "https://sheetdb.io/api/v1/u2bi74veb32hq",
   whatsapp: "" // رقم واتساب اختياري، مثال: "213555000000"
 };
